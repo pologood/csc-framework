7ee95f0d4f5d8e7eeb7e91fadc493b628bd4aa84
csophys <zjsy_cs@163.com>
2015-12-23 15:24:36 +0800

    now can use new csc-common-service

---
+++ csc-framework-service/src/test/java/com/dianping/demo/dao/PeopleService.java
@@ -0,0 +1,53 @@
+package com.dianping.demo.dao;
+
+import com.dianping.csc.common.service.util.SpringBaseWithCustomizeRunnerTest;
+import com.dianping.demo.entity.PeopleEntity;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.transaction.AfterTransaction;
+import org.springframework.test.context.transaction.BeforeTransaction;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by csophys on 15/5/15.
+ */
+
+
+public class PeopleService extends SpringBaseWithCustomizeRunnerTest {
+
+    @Resource
+    PeopleDao peopleDao;
+
+    /**
+     * 默认开启事务自动回滚
+     */
+    @Test
+    public void add() {
+        Integer result = peopleDao.insert(new PeopleEntity("csophys", "男", 42));
+        Assert.assertTrue(result > 0);
+        System.out.println("rollback "+result);
+    }
+
+    /**
+     * 不需要事务自动回滚
+     */
+    @Test
+    @Rollback(false)
+    public void addWithouRollback() {
+        Integer result = peopleDao.insert(new PeopleEntity("csophys", "男", 42));
+        Assert.assertTrue(result > 0);
+        System.out.println("nonRollback:"+result);
+    }
+
+    @BeforeTransaction
+    public void before() {
+        System.out.println("before");
+    }
+
+    @AfterTransaction
+    public void after() {
+        System.out.println("after");
+    }
+}
